49a78558d904282efc64db4e201a5062a050b426
passioneffort <agronkercishta.ai@gmail.com>
2023-08-04 12:03:04 -0400

    update

--- lib/services/remote_service_video_list.dart
+++ lib/services/remote_service_video_list.dart
@@ -9,19 +9,5 @@ class RemoteService_list {
     var uri =
         Uri.parse('http://mirror.ccjjj.com/xcxapi/teacher/video_list?uid=777');
     var response = await client.post(uri);
-
-    if (response.statusCode == 200) {
-      var str = response.body;
-      PostList res = postFromJson(str);
-
-      print(
-          '------------------------success_post_api_list---------------------------');
-
-      return res;
-    } else {
-      print(
-          '------------------------failed_post_api_list---------------------------');
-      return null; // or an empty list: return [];
-    }
   }
 }
