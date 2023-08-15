c435cbf57db77319e00c12451d6a2ee379f736cf
passioneffort <agronkercishta.ai@gmail.com>
2023-08-15 16:33:35 -0400

    modify

--- lib/models/video_list_post.dart
+++ lib/models/video_list_post.dart
@@ -142,7 +142,7 @@ class EnumValues<T> {
   Map<String, T> map;
   late Map<T, String> reverseMap;
 
-  // EnumValues(this.map);
+  EnumValues(this.map);
 
   Map<T, String> get reverse {
     reverseMap = map.map((k, v) => MapEntry(v, k));
