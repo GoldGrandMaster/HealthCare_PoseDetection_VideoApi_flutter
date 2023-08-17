7c67ca82ce957197da0156bbc56f989317316b32
passioneffort <agronkercishta.ai@gmail.com>
2023-08-17 04:01:46 -0400

    update

--- lib/helpers/app_config.dart
+++ lib/helpers/app_config.dart
@@ -25,17 +25,17 @@ class App {
         _width - (_queryData.padding.left + _queryData.padding.right) / 100.0;
   }
 
-  // double appHeight(double v) {
-  //   return _height * v;
-  // }
+  double appHeight(double v) {
+    return _height * v;
+  }
 
-  // double appWidth(double v) {
-  //   return _width * v;
-  // }
+  double appWidth(double v) {
+    return _width * v;
+  }
 
-  // double appVerticalPadding(double v) {
-  //   return _heightPadding * v;
-  // }
+  double appVerticalPadding(double v) {
+    return _heightPadding * v;
+  }
 
   double appHorizontalPadding(double v) {
 //    int.parse(settingRepo.setting.mainColor.replaceAll("#", "0xFF"));
@@ -43,12 +43,12 @@ class App {
   }
 }
 
-// class Colors {
-//   Color mainColor(double opacity) {
-//     try {
-//       return Color(settingRepo.setting.value.mainColor.value);
-//     } catch (e) {
-//       return Color(4293943954);
-//     }
-//   }
-// }
+class Colors {
+  Color mainColor(double opacity) {
+    try {
+      return Color(settingRepo.setting.value.mainColor.value);
+    } catch (e) {
+      return Color(4293943954);
+    }
+  }
+}
