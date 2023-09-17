84f608ff2d3cf860f9abfdd888a064c2f63d96bc
passioneffort <agronkercishta.ai@gmail.com>
2023-09-17 10:13:13 -0400

    modify

--- lib/pages/splash_screen.dart
+++ lib/pages/splash_screen.dart
@@ -26,42 +26,42 @@ class _SplashScreenState extends State<SplashScreen>
     });
   }
 
-  @override
-  Widget build(BuildContext context) {
-    final width = MediaQuery.of(context).size.width;
-    final height = MediaQuery.of(context).size.height;
-    return Scaffold(
-      body: Stack(
-        alignment: AlignmentDirectional.topCenter,
-        children: [
-          Positioned(
-            width: width,
-            top: height / 3,
-            // child: Container(
-            child: Center(
-              child: Container(
-                child: Text(
-                  "体能训练",
-                  style: TextStyle(
-                    color: Color(0xff3c8758),
-                    fontSize: 40,
-                  ),
-                ),
-              ),
-            ),
-            // ),
-          ),
-          Positioned(
-            width: width - 80,
-            bottom: height / 8,
-            child: Center(
-              child: Container(
-                child: Text("©2023 Richard Digital Healthcare."),
-              ),
-            ),
-          )
-        ],
-      ),
-    );
-  }
+  // @override
+  // Widget build(BuildContext context) {
+  //   final width = MediaQuery.of(context).size.width;
+  //   final height = MediaQuery.of(context).size.height;
+  //   return Scaffold(
+  //     body: Stack(
+  //       alignment: AlignmentDirectional.topCenter,
+  //       children: [
+  //         Positioned(
+  //           width: width,
+  //           top: height / 3,
+  //           // child: Container(
+  //           child: Center(
+  //             child: Container(
+  //               child: Text(
+  //                 "体能训练",
+  //                 style: TextStyle(
+  //                   color: Color(0xff3c8758),
+  //                   fontSize: 40,
+  //                 ),
+  //               ),
+  //             ),
+  //           ),
+  //           // ),
+  //         ),
+  //         Positioned(
+  //           width: width - 80,
+  //           bottom: height / 8,
+  //           child: Center(
+  //             child: Container(
+  //               child: Text("©2023 Richard Digital Healthcare."),
+  //             ),
+  //           ),
+  //         )
+  //       ],
+  //     ),
+  //   );
+  // }
 }
